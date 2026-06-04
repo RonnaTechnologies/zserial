bf56ce405284abcfa10a8ec3e10cdf3d9b8298de
RonnaTechnologies <contact@ronnatech.com>
2026-06-04 17:33:34 +0200

    read device idVendor (linux)

--- src/main.zig
+++ src/main.zig
@@ -3,14 +3,45 @@ const std = @import("std");
 const linuxSerialRoot: []const u8 = "/sys/class/tty";
 const linuxAllowedDevices = [_][]const u8{ "ttyS", "ttyUSB", "ttyXRUSB", "ttyACM", "ttyAMA", "rfcomm", "ttyAP", "ttyGS" };
 
-fn arrayContains(comptime T: type, needle: []const T, haystack: anytype) bool {
-    for (haystack) |value| {
-        if (std.mem.startsWith(T, needle, value)) return true;
-    }
-    return false;
+fn fileExists(io: std.Io, path: []const u8) bool {
+    return !std.meta.isError(std.Io.Dir.cwd().access(io, path, .{}));
 }
 
-pub fn listSerialPorts(
+// fn FilterIterator(comptime T: type) type {
+//     return struct {
+//         items: []const T,
+//         index: usize = 0,
+//         predicate: *const fn (T) bool,
+
+//         pub fn next(self: *@This()) ?T {
+//             while (self.index < self.items.len) {
+//                 const item = self.items[self.index];
+//                 self.index += 1;
+//                 if (self.predicate(item)) return item;
+//             }
+//             return null;
+//         }
+//     };
+// }
+
+fn linuxIsValidPort(comptime T: type, needle: []const T, haystack: anytype) bool {
+    const Haystack = @TypeOf(haystack);
+    switch (@typeInfo(Haystack)) {
+        .pointer => {
+            for (haystack) |value| {
+                if (std.mem.startsWith(T, needle, value)) {
+                    return true;
+                }
+            }
+            return false;
+        },
+        else => {
+            @compileError("Invalid container type.");
+        },
+    }
+}
+
+fn listSerialPorts(
     io: std.Io,
     allocator: std.mem.Allocator,
     comptime path: []const u8,
@@ -22,7 +53,7 @@ pub fn listSerialPorts(
 
     while (try dirIterator.next(io)) |dirContent| {
         const name = try std.fmt.allocPrint(allocator, "{s}/{s}", .{ path, dirContent.name });
-        if (arrayContains(u8, dirContent.name, &linuxAllowedDevices)) {
+        if (linuxIsValidPort(u8, dirContent.name, &linuxAllowedDevices)) {
             try ports.append(allocator, name);
         }
     }
@@ -40,7 +71,36 @@ pub fn main(init: std.process.Init) !void {
 
     const ports = try listSerialPorts(io, arenaAllocator, linuxSerialRoot);
 
-    for (ports.items) |name| {
-        std.debug.print("{s}\n", .{name});
+    for (ports.items) |port| {
+        const devicePath = try std.fmt.allocPrint(arenaAllocator, "{s}/device", .{port});
+
+        const realPath = try std.Io.Dir.cwd().realPathFileAlloc(io, devicePath, arenaAllocator);
+        const parentPath = std.fs.path.dirname(realPath) orelse "/";
+
+        const vendorPath = try std.fmt.allocPrint(arenaAllocator, "{s}/idVendor", .{parentPath});
+        const hasVendor = fileExists(io, vendorPath);
+
+        // std.debug.print("path = {s}, has vendor: {}\n", .{ parentPath, hasVendor });
+
+        if (hasVendor) {
+            var buf: [16]u8 = undefined;
+
+            const file = try std.Io.Dir.cwd().openFile(io, vendorPath, .{});
+            defer file.close(io);
+
+            var reader = file.reader(io, &buf);
+
+            const n = try reader.interface.readSliceShort(&buf);
+
+            const vendorStr = std.mem.trim(u8, buf[0..n], " \t\r\n");
+            // std.log.info("vendor id = \"{s}\"", .{vendorStr});
+            const vendor_id = try std.fmt.parseInt(u32, vendorStr, 16);
+
+            std.log.info("path = {s}, vendor_id = 0x{x}", .{ parentPath, vendor_id });
+        }
     }
+
+    // for (ports.items) |name| {
+    // std.debug.print("{s}\n", .{name});
+    // }
 }

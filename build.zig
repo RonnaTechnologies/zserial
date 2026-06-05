ab7f0ac146fd1ca620709ff5edfd385cbf9bf818
RonnaTechnologies <contact@ronnatech.com>
2026-06-05 21:06:20 +0200

    prepare build.zig for windows

--- build.zig
+++ build.zig
@@ -15,40 +15,6 @@ pub fn build(b: *std.Build) void {
         "path to macOS SDK",
     );
 
-    const effective_os = target.query.os_tag orelse .linux;
-    const is_macos = (effective_os == .macos);
-
-    var sdk_path: ?[]const u8 = null;
-
-    if (is_macos) {
-        sdk_path = osxcross_sdk orelse
-            @panic("missing -Dosxcross-sdk");
-
-        if (b.sysroot == null)
-            b.sysroot = sdk_path;
-    }
-
-    const translate_c = b.addTranslateC(.{
-        .root_source_file = b.path("src/macos.h"),
-        .target = target,
-        .optimize = optimize,
-        .link_libc = true,
-    });
-
-    if (is_macos) {
-        const sdk = std.Build.LazyPath{
-            .cwd_relative = sdk_path.?,
-        };
-
-        // IMPORTANT: this is what fixes header lookup
-        translate_c.addIncludePath(sdk.path(b, "usr/include"));
-        translate_c.addSystemFrameworkPath(
-            sdk.path(b, "System/Library/Frameworks"),
-        );
-    }
-
-    const c_module = translate_c.createModule();
-
     const exe = b.addExecutable(.{
         .name = "zserial",
         .root_module = b.createModule(.{
@@ -59,36 +25,56 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
+    const effective_os = target.query.os_tag orelse .linux;
+
+    switch (effective_os) {
+        .macos => {
+            var sdk_path: ?[]const u8 = null;
+            sdk_path = osxcross_sdk orelse
+                @panic("missing -Dosxcross-sdk");
+
+            if (b.sysroot == null)
+                b.sysroot = sdk_path;
+            const sdk = std.Build.LazyPath{
+                .cwd_relative = sdk_path.?,
+            };
+
+            const translate_c = b.addTranslateC(.{
+                .root_source_file = b.path("src/macos.h"),
+                .target = target,
+                .optimize = optimize,
+                .link_libc = true,
+            });
+            translate_c.addIncludePath(sdk.path(b, "usr/include"));
+            translate_c.addSystemFrameworkPath(
+                sdk.path(b, "System/Library/Frameworks"),
+            );
+            const c_module = translate_c.createModule();
+            exe.root_module.addImport("c", c_module);
+
+            exe.root_module.addSystemFrameworkPath(
+                sdk.path(b, "System/Library/Frameworks"),
+            );
+
+            exe.root_module.linkFramework("IOKit", .{});
+            exe.root_module.linkFramework("CoreFoundation", .{});
+        },
+        else => {
+            const run_cmd = b.addRunArtifact(exe);
+
+            run_cmd.step.dependOn(b.getInstallStep());
+
+            if (b.args) |args| {
+                run_cmd.addArgs(args);
+            }
+
+            b.step("run", "Run the app").dependOn(&run_cmd.step);
+        },
+    }
+
     exe.root_module.addOptions("buildOptions", options);
 
-    // macOS linking
-    if (is_macos) {
-        exe.root_module.addImport("c", c_module);
-        const sdk = std.Build.LazyPath{
-            .cwd_relative = sdk_path.?,
-        };
-
-        exe.root_module.addSystemFrameworkPath(
-            sdk.path(b, "System/Library/Frameworks"),
-        );
-
-        exe.root_module.linkFramework("IOKit", .{});
-        exe.root_module.linkFramework("CoreFoundation", .{});
-    }
-
     b.installArtifact(exe);
-
-    if (!is_macos) {
-        const run_cmd = b.addRunArtifact(exe);
-
-        run_cmd.step.dependOn(b.getInstallStep());
-
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
-        }
-
-        b.step("run", "Run the app").dependOn(&run_cmd.step);
-    }
 
     // docs
     const emit_docs = b.addSystemCommand(&.{

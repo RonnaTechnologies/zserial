9532072ae6d791157dc42dfe81a9ec296b941d16
RonnaTechnologies <contact@ronnatech.com>
2026-06-07 21:52:07 +0200

    add port options to common.zig

--- src/common.zig
+++ src/common.zig
@@ -9,3 +9,20 @@ pub const PortInfo = struct {
     pid: u16,
     location: []const u8,
 };
+
+pub const Options = struct {
+    // Baud rate
+    baudRate: u32 = undefined,
+    // Data bits
+    dataBits: DataBits = .eight,
+    // Stop bits
+    stopBits: StopBits = .one,
+    // Parity
+    parity: Parity = .none,
+    // Non-blocking mode
+    nonBlocking: bool = false,
+};
+
+pub const DataBits = enum { five, six, seven, eight };
+pub const StopBits = enum { one, two };
+pub const Parity = enum { none, odd, even };

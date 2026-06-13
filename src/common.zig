ab15fee147e45c10aeb3417f9b2ea0fdfa9ee6ff
RonnaTechnologies <contact@ronnatech.com>
2026-06-13 21:41:58 +0200

    add ReadStrategy to common

--- src/common.zig
+++ src/common.zig
@@ -11,16 +11,26 @@ pub const PortInfo = struct {
 };
 
 pub const Options = struct {
-    // Baud rate
+    /// Baud rate
     baudRate: u32 = undefined,
-    // Data bits
+    /// Data bits
     dataBits: DataBits = .eight,
-    // Stop bits
+    /// Stop bits
     stopBits: StopBits = .one,
-    // Parity
+    /// Parity
     parity: Parity = .none,
-    // Hardware flow control
+    /// Hardware flow control
     hardwareFlowControl: bool = false,
+};
+
+pub const ReadStrategy = union(enum) {
+    /// Block until at least 'nBbytes' arrive, or 'timeout_ms' elapses.
+    /// If timeout_ms is null, blocks indefinitely.
+    blockingMinTimeout: struct { nBytes: usize, timeout_ms: ?u32 },
+    /// Block until at least one byte arrives, or 'timeout_ms' elapses.
+    blockingAnyTimeout: struct { timeout_ms: u32 },
+    /// Non-blocking read. Returns immediately with available data (0 or more bytes).
+    nonBlocking,
 };
 
 pub const DataBits = enum(u8) { five = 5, six = 6, seven = 7, eight = 8 };

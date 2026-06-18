ca7361098cfbee8f6f1933c6b6869a3326e89802
RonnaTechnologies <contact@ronnatech.com>
2026-06-18 22:40:20 +0200

    windows: parseUsbHardwareInfo (partial)

--- src/windows.zig
+++ src/windows.zig
@@ -1,8 +1,6 @@
 const std = @import("std");
 const c = @import("c");
 
-const SPDRP_HARDWAREID: c_ulong = 0x0001;
-
 pub const port = @import("common.zig");
 
 pub const baudRates: []const u32 = &.{
@@ -171,7 +169,7 @@ fn enumGuids(
                 // defer allocator.free(szHardwareIdStr);
             }
 
-            const info = port.PortInfo{
+            var info = port.PortInfo{
                 .device = try allocator.dupe(u8, portName),
                 .product = try allocator.dupe(u8, ""),
                 .manufacturer = try allocator.dupe(u8, ""),
@@ -182,10 +180,29 @@ fn enumGuids(
             };
 
             if (std.mem.startsWith(u8, szHardwareIdStr, "USB")) {
-                std.log.warn("{s}: {s}", .{ info.device, info.location });
+                try parseUsbHardwareInfo(szHardwareIdStr, &info);
+
+                std.log.debug("vid = {d}, pid = {d}", .{ info.vid, info.pid });
             }
 
             _ = ports;
         }
     }
 }
+
+fn parseUsbHardwareInfo(info: []const u8, portInfo: *port.PortInfo) !void {
+    const vidStr = "VID_";
+    const vidOffset = (std.mem.indexOf(u8, info, vidStr) orelse return error.ParseError) + vidStr.len;
+
+    const vid = std.fmt.parseInt(u16, info[vidOffset .. vidOffset + 4], 16) catch return error.ParseError;
+
+    const pidStr = "PID_";
+    const pidOffset = (std.mem.indexOf(u8, info, pidStr) orelse return error.ParseError) + pidStr.len;
+
+    const pid = std.fmt.parseInt(u16, info[pidOffset .. pidOffset + 4], 16) catch return error.ParseErrro;
+
+    portInfo.vid = vid;
+    portInfo.pid = pid;
+}
+
+const SPDRP_HARDWAREID: c_ulong = 0x0001;
